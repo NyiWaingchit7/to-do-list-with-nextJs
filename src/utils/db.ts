6f918d5cdb9e3688acd75109feab121b5dde1145
Your Name <you@example.com>
2023-11-10 13:58:04 +0630

    to-do-list

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from "@prisma/client";
+declare global {
+  var prisma: PrismaClient; // This must be a `var` and not a `let / const`
+}
 
-export const prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
